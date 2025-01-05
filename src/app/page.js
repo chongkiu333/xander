c2afa97ca0257fedb3f2aa4732941bd6ef018515
chongkiu333 <xingshanhe33@gmail.com>
2025-01-05 18:10:07 +0800

    change

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ import { EffectComposer, Outline , Select , Selection } from "@react-three/postp
 import styles from './page.module.css';
 
 
-function RingModel({modelPath, position,scale=1, ...props}){
+function RingModel({modelPath, position,scale=1,text, ...props}){
   const { nodes, materials } = useGLTF(modelPath)
   const modelRef = useRef();
 
@@ -34,9 +34,21 @@ function RingModel({modelPath, position,scale=1, ...props}){
         scale={scale}
         position={position}
       />
+
+      <Html
+        position={[position[0], position[1] + 1, position[2]]} // 在模型上方 2 个单位
+        center 
+        distanceFactor={0.01} 
+      >
+        <div className={styles.subTitle} >
+          {text}
+        </div>
+      </Html>
     </group>
   )
 }
+
+
 
 function GLTFModel({ modelPath, position = [0, 0, 0], scale = 1 }) {
   const gltf = useLoader(GLTFLoader, modelPath);
@@ -71,25 +83,54 @@ function GLTFModel({ modelPath, position = [0, 0, 0], scale = 1 }) {
   );
 }
 
+function RotatingGroup() {
+  const groupRef = useRef();
 
+  // 使用 useFrame 实现父级 group 的旋转
+  useFrame((state, delta) => {
+    if (groupRef.current) {
+      groupRef.current.rotation.y += delta * 0.01; // 缓慢增加 Y 轴旋转
+    }
+  });
+
+  return (
+    <group ref={groupRef}>
+      {/* 将所有 RingModel 放在同一个 group 中 */}
+      <RingModel modelPath="/model2/ring1.gltf" position={[1, 8, -4.5]} text="BackStage" />
+      <RingModel modelPath="/model2/ring2.gltf" position={[10, 4, 6]} scale={1.15} text="CreativeDirection" />
+      <RingModel modelPath="/model2/ring3.gltf" position={[-5, -2, 4.5]} text="Music" />
+      <RingModel modelPath="/model2/ring4.gltf" position={[-11, -8, -5]} scale={2} text="Video" />
+    </group>
+  );
+}
 
 export default function Home() {
-  const [hovered, setHovered] = useState(false);
-
+  const [show, setShow] = useState(false);
+  
 
 return (
   <div className="canvasContainer" >
-    <div
-    className={`${styles.title} ${hovered ? styles.hovered : ""}`}
-        onPointerOver={() => setHovered(true)} // 鼠标移入
-        onPointerOut={() => setHovered(false)} // 鼠标移出
-    
-     >
-     
-      XanderGhost
+
+    <div className={styles.headContainer}>
+      <span style={{position: 'absolute'}}>
+    <div className={styles.title} onClick={() => setShow(!show)}  >
+      XanderGhost<div className={styles.point}></div> {show &&<div style={{marginLeft: '5px'}}>about</div>}
       
+    </div>
+
+    {show && <div className={styles.about}>
+    Xander Ghost epitomizes the quintessential modern renaissance creative, seamlessly blending his talents across various disciplines. With a decade-long tenure in the realms of design, music, and creative direction, he has become a formidable force in the industry.
+    <br/>
+    In 2018, Xander unveiled his visionary eyewear brand, A Better Feeling, serving as a testament to his commitment to pioneering excellence.
+    <br/>
+    Mirroring the avant-garde essence of his music, the brand transcends conventional norms, seamlessly intertwining fashion and art. A Better Feeling has swiftly risen to prominence, adorning the shelves of esteemed retailers worldwide, including Ssense, Selfridges, Louisa Via Roma, Lncc, Level Shoes, and numerous others.
+    <br/>
+    Notably, Xander's creations have captured the attention of a global audience, including acclaimed celebrities such as Travis Scott, Billie Eilish, Rosalia, and Burna Boy. Through his unparalleled fusion of creativity and innovation, Xander Ghost continues to leave an indelible mark on the cultural landscape, inspiring and captivating audiences around the globe.
+    </div>}
+
+    </span>
+     
      </div>
-    
     <Canvas className={styles.canvas}  style={{ height: '100%'}} alpha={false}>
 
       <Selection>
@@ -112,11 +153,8 @@ return (
     <GLTFModel modelPath="/model/ring2.gltf" position={[-3, -2, 4.5]} scale={0.8} />
     <GLTFModel modelPath="/model/ring4.gltf" position={[-9, -8, -4]} scale={1.2} /> */}
 
-        <RingModel modelPath={"/model2/ring1.gltf"} position={[1, 8, -4.5]} />
-        <RingModel modelPath={"/model2/ring2.gltf"} position={[10, 4, 6]} scale={1.2} />
-        <RingModel modelPath={"/model2/ring3.gltf"} position={[-5, -2, 4.5]} />
-        <RingModel modelPath={"/model2/ring4.gltf"} position={[-11, 8, 5]} rotation={[-Math.PI, 0, 0]} scale={2} />
-        
+
+<RotatingGroup />
       <ambientLight intensity={10} />
       <directionalLight color="white" position={[1, 3, 5]} />
                 
